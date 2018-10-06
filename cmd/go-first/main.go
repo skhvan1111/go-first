c9abcb6161a026d130b13d875ac77fb90aca2abc
Sergey Khvan <sergey.khvan@quadevs.com>
2018-10-06 11:44:10 +0300

    diags

--- cmd/go-first/main.go
+++ cmd/go-first/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	log.Print("Hello, World")
 
-	err := http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
+
+	go func() {
+		err := http.ListenAndServe(":8081", router)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+
+	diagnostics1 := diagnostics.NewRouter()
+	err := http.ListenAndServe(":8585", diagnostics1)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
---
+++ internal/diagnostics/diagnostics.go
@@ -0,0 +1,24 @@
+package diagnostics
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func NewDiagnostics() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/health", health)
+	router.HandleFunc("/ready", ready)
+
+	return router
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
+}
+
+func ready(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
+}
